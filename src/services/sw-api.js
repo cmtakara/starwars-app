adc297cc52a7fe664c6cc1dfc1c24277a37f484e
Christina Takara <christina.takara@gmail.com>
2022-01-21 20:31:42 -0600

    updated working files

---
+++ src/services/sw-api.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react'
+
+// eventually add in
+// const [starShips, setStarShips] = useSate([])
+// const fetchShip = asyn () => {
+//     try {
+//         const response = awai axios.get (... API)
+//         setStarShips(response.data.results)
+//         console.log(starShips)
+//     } catch (error) {
+//         console.error(error)
+
+//     }
+// }
+// useEffect(() => { fetchShip() }}, []);
+
+const SwApi = () => {
+
+
+    return (
+        <div>
+            SWAPI            
+        </div>
+    );
+}
+
+export default SwApi;
